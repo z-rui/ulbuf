57f3ec68d6fc135779160211561bcd4471cd5510
张睿 <zrui16@hotmail.com>
2015-08-17 17:27:57 +0800

    Add a macro guard in ulbuf.h

--- ulbuf.h
+++ ulbuf.h
@@ -1,3 +1,6 @@
+#ifndef ULBUF_H
+#define ULBUF_H
+
 #include <stddef.h>
 
 extern char *bufnew(size_t size);
@@ -5,3 +8,5 @@ extern char *bufext(char **base, char *tail, size_t n);
 extern char *bufncat(char **base, char *tail, const char *s, size_t);
 extern char *bufcat(char **base, char *tail, const char *s);
 extern void bufdel(char *buf);
+
+#endif
